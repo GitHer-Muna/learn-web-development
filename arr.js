635216a3245993839fd662397774a11dc318dd07
GitHer-Muna <muna.bhattarai@study.lbef.edu.np>
2024-07-06 00:07:55 +0545

    first commit

---
+++ arr.js
@@ -0,0 +1,4 @@
+const myArray = ['hello', 'world', 'welcome'];
+
+var collage=["LBEF","Sunway","PCPS","Pulchok"];
+var collage=new Array("LBEF","Sunway","PCSC","Pulchok");
---
+++ array.js
@@ -0,0 +1,8 @@
+int a=10
+float b=10.5
+string collage="LBEF"
+
+a=10,5,20,30,40,50;
+
+var collage=["LBEF","Sunway","PCPS","Pulchok"];
+var collage=new Array("LBEF","Sunway","PCSC","Pulchok");
